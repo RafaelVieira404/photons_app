534dae09ef097af522537a59dee173c1d871a2be
Rafael Vieira <rafavs09@gmail.com>
2025-02-13 00:16:34 -0300

    Save progress

--- app/src/main/java/com/example/android_photonsapp/MainActivity.java
+++ app/src/main/java/com/example/android_photonsapp/MainActivity.java
@@ -1,6 +1,17 @@
 package com.example.android_photonsapp;
 
+import android.graphics.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +20,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    CameraManager cManager;
+    Size size;
+    String cameraId;
+    private Object CameraMetadata;
+    Handler backGroundHandler;
+    HandlerThread handlerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void openCamera() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if()
+        }
+    }
+
+    private void openBackgroundHandler() {
+        HandlerThread handlerThread = new HandlerThread("camera_app");
+        handlerThread.start();
+        backGroundHandler = new Handler(handlerThread.getLooper());
+    }
+
+    private void stopBackgroundHandler() {
+        handlerThread.quit();
+        handlerThread = null;
+        backGroundHandler = null;
+    }
+
+    private void setupCamera() {
+        cManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            String[] cameraIds = cManager.getCameraIdList();
+
+            for (String cameraId : cameraIds) {
+                CameraCharacteristics cameraCharacteristics = cManager.getCameraCharacteristics(cameraId);
+
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == android.hardware.camera2.CameraMetadata.LENS_FACING_FRONT) {
+                    this.cameraId = cameraId;
+                    StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    size = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+                }
+
+
+            }
+        } catch (CameraAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
